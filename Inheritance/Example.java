a2674036d3cbc52ac7f2071c667e1e08bb3e8163
AakarshitAgarwal <talktoshanuagarwal@gmail.com>
2020-01-22 23:48:51 +0530

    4th commit

---
+++ Inheritance/Example.java
@@ -0,0 +1,14 @@
+class Example
+{
+ public static void main(String args[])
+  { 
+    Student s1=new Student();
+    s1.setRollno(100);
+    s1.setName("Rahul");
+    s1.setAge(18);
+    System.out.println("Roll no. "+s1.getRollno());
+    System.out.println("Name "+s1.getName());
+    System.out.println("Age "+s1.getAge());
+  }
+}
+    
