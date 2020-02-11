ab5c87c74b8633ec205c9f437b8c21469d703174
AakarshitAgarwal <talktoshanuagarwal@gmail.com>
2020-02-12 00:17:57 +0530

    Adding

--- Run_time_Polymorphism/Polymorphism.java
+++ Run_time_Polymorphism/Polymorphism.java
@@ -1,6 +1,7 @@
 //Run-time polymorphism,Dynamic Method Dispatch
 class A
 {
+  int i=10;
   public void m1()  //static keyword should not be used
   {
     System.out.println("Class A");
@@ -8,6 +9,7 @@ class A
 }
 class B extends A  
 {
+  int i=20;
   public void m1()
   {
     System.out.println("Class B");
@@ -19,8 +21,12 @@ public class Polymorphism
   {
   A obj=new A();
   A obj1=new B();
+  A obj2=new A();
+  A obj3=new B();
   obj.m1();   //class A
   obj1.m1();  //class B
+  System.out.println(obj2.i);
+  System.out.println(obj3.i);  
   }
 }  
 
