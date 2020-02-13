dce59d2f5bdedd1594d4cf33f4223478b020dd1e
AakarshitAgarwal <talktoshanuagarwal@gmail.com>
2020-02-13 23:19:15 +0530

    Adding

---
+++ Scanner_Class/InputExample.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class InputExample
+{
+  public static void main(String []args){
+  System.out.println("Enter your name and age");
+  Scanner sc =new Scanner(System.in);
+  String name=sc.next();
+  int age=sc.nextInt();
+  System.out.println("Name: "+name+",Age :"+age);
+  }
+}
+  
+  
