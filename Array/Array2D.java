52ce54bae02c9d1bdae4c251f097aa0ba7db6d28
AakarshitAgarwal <talktoshanuagarwal@gmail.com>
2020-02-15 23:41:28 +0530

    Updating

---
+++ Array/Array2D.java
@@ -0,0 +1,11 @@
+public class Array2D
+{
+public static void main(String args[])
+ {
+   int [][]a=new int[4][];
+   a[0]=new int[5];
+   a[1]=new int[3];
+   System.out.println("Length or array connected to first index is="+a[1].length);
+  }
+}
+  
