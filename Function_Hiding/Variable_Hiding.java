e752ca125d87d4b73fc8d84b4b25cb138d8bf5d0
AakarshitAgarwal <talktoshanuagarwal@gmail.com>
2020-02-17 23:41:11 +0530

    Ädding

---
+++ Function_Hiding/Variable_Hiding.java
@@ -0,0 +1,16 @@
+class Parent
+{
+   static int y=4;
+}
+class Child extends Parent
+{
+ static
+ {y=5};
+}
+public class Variable_Hiding
+{
+ public static void main(String args[])
+  {
+   System.out.println("y="+Child.y);  //y=4
+  }
+}
